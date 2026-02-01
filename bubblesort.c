8f6438d6615267ed77fd6ef55ccc14fc1016a2ee
Huarui <617639437@qq.com>
2026-02-01 15:17:20 +0800

    Add bubblesort.c - implementation of bubble sort algorithm

---
+++ bubblesort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// 冒泡排序函数
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// 打印数组函数
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// 主函数
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("排序前的数组：\n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("\n排序后的数组：\n");
+    printArray(arr, n);
+
+    return 0;
+}
